c6df26c4fa0ec6fee0323faf9febca35c13cc8c5
tousifanaam <64709584+tousifanaam@users.noreply.github.com>
2021-08-05 10:52:45 -0500

    Add files via upload

--- sudoku.py
+++ sudoku.py
@@ -2,6 +2,18 @@ import string
 import json
 from random import choice, shuffle
 from os import system, name
+from time import time
+
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        t1 = time()
+        res = func(*args, **kwargs)
+        t2 = time()
+        print(f"{func.__name__}(): {t2 - t1} seconds.")
+        return res
+    return wrapper
+
 
 def rm(filename: str):
     if name == 'nt':
@@ -9,116 +21,121 @@ def rm(filename: str):
     else:
         _ = system('rm -rf ' + filename)
 
+
 class Sudoku:
-	"""A Sudoku 9*9 Board"""
-	
-	def __init__(self, board):
-		"""Initializing basic attributes"""
-		self.board = board
-		self.allpos = self.all_pos()
-		self.empos = self.empty_pos()
-		self.columns = self._columns()
-		self.toprank, self.middlerank, self.bottomrank = self.ranks()
-		self.block1, self.block2, self.block3, self.block4, self.block5, self.block6, self.block7, self.block8, self.block9 = self.blocks()
-		self.leftstack, self.centrestack, self.rightstack = self.stacks()
+    """A Sudoku 9*9 Board"""
 
-	def __str__(self):
-		return self.display(self.board) + "\n"
+    def __init__(self, board: list[list]) -> None:
+        """Initializing basic attributes"""
+        self.board = board
+        self.allpos = self.all_pos()
+        self.empos = self.empty_pos()
+        self.columns = self._columns()
+        self.toprank, self.middlerank, self.bottomrank = self.ranks()
+        self.block1, self.block2, self.block3, self.block4, self.block5, self.block6, self.block7, self.block8, self.block9 = self.blocks()
+        self.leftstack, self.centrestack, self.rightstack = self.stacks()
 
-	def all_pos(self):
-		"""Positioning all values"""
-		all_p = [] # (x, y, z) = (board[index], baord[index][index], item)
-		for i in range(0,9):
-		    row = self.board[i]
-		    for n in range(0,9):
-		        item = row[n]
-		        all_p.append((i, n, item))
-		return all_p
+    def __str__(self) -> str:
+        return self.display(self.board) + "\n"
 
-	def empty_pos(self, emp = 0):
-		"""Determining the empty values"""
-		empty = [] # [x, y] = [board[index], baord[index][index]]
-		for i in self.allpos:
-		    a, b, c = i
-		    if c == emp:
-		        empty.append((a, b))
-		return empty
+    def __repr__(self) -> str:
+        return "Sudoku(" + str(self.board) + ")"
 
-	def _columns(self):
-		res = [[] for _ in range(9)]
-		for i in self.board:
-			for n in range(len(i)):
-				res[n].append(i[n])
-		return res
+    def all_pos(self) -> list[tuple]:
+        """Positioning all values"""
+        all_p = []  # (x, y, z) = (board[index], baord[index][index], item)
+        for i in range(0, 9):
+            row = self.board[i]
+            for n in range(0, 9):
+                item = row[n]
+                all_p.append((i, n, item))
+        return all_p
 
-	@staticmethod
-	def display(board: list) -> str:
-	    dis = ""
-	    for i in range(len(board)):
-	        row = board[i]
-	        line = ""
-	        for i in range(len(row)):
-	            line += str(row[i]).replace("0", " ") + " "
-	            if i == 2 or i == 5:
-	                line += "| "
-	        line += "\n"
-	        dis += line
-	    dis_l = dis.split("\n")
-	    dis = ""
-	    for i in range(len(dis_l)):
-	        check = dis_l[i]
-	        dis += check
-	        dis += "\n"
-	        if i == 2 or i == 5:
-	            dis += "------|-------|------"
-	            dis += "\n"
-	    return dis.strip("\n")
+    def empty_pos(self, emp = 0) -> list[tuple]:
+        """Determining the empty values"""
+        empty = []  # [x, y] = [board[index], baord[index][index]]
+        for i in self.allpos:
+            a, b, c = i
+            if c == emp:
+                empty.append((a, b))
+        return empty
 
-	def ranks(self):
-		board = self.board
-		top_rank = []
-		middle_rank = []
-		bottom_rank = []
-		for i in range(0,9):
-		    row = board[i]
-		    if i < 3:
-		        top_rank.append(row)
-		    elif i < 6:
-		        middle_rank.append(row)
-		    else:
-		        bottom_rank.append(row)
+    def _columns(self) -> list[list]:
+        res = [[] for _ in range(9)]
+        for i in self.board:
+            for n in range(len(i)):
+                res[n].append(i[n])
+        return res
 
-		return top_rank, middle_rank, bottom_rank
+    @staticmethod
+    def display(board: list) -> str:
+        dis = ""
+        for i in range(len(board)):
+            row = board[i]
+            line = ""
+            for i in range(len(row)):
+                line += str(row[i]).replace("0", " ") + " "
+                if i == 2 or i == 5:
+                    line += "| "
+            line += "\n"
+            dis += line
+        dis_l = dis.split("\n")
+        dis = ""
+        for i in range(len(dis_l)):
+            check = dis_l[i]
+            dis += check
+            dis += "\n"
+            if i == 2 or i == 5:
+                dis += "------|-------|------"
+                dis += "\n"
+        return dis.strip("\n")
 
-	def blocks(self):
-		top_rank, middle_rank, bottom_rank = self.ranks()
-		b1 = [top_rank[0][0:3], top_rank[1][0:3], top_rank[2][0:3]]
-		b2 = [top_rank[0][3:6], top_rank[1][3:6], top_rank[2][3:6]]
-		b3 = [top_rank[0][6:9], top_rank[1][6:9], top_rank[2][6:9]]
-		b4 = [middle_rank[0][0:3], middle_rank[1][0:3], middle_rank[2][0:3]]
-		b5 = [middle_rank[0][3:6], middle_rank[1][3:6], middle_rank[2][3:6]]
-		b6 = [middle_rank[0][6:9], middle_rank[1][6:9], middle_rank[2][6:9]]
-		b7 = [bottom_rank[0][0:3], bottom_rank[1][0:3], bottom_rank[2][0:3]]
-		b8 = [bottom_rank[0][3:6], bottom_rank[1][3:6], bottom_rank[2][3:6]]
-		b9 = [bottom_rank[0][6:9], bottom_rank[1][6:9], bottom_rank[2][6:9]]
-		blocks = (
-		    b1, b2, b3,
-		    b4, b5, b6,
-		    b7, b8, b9,
-		)
-		return blocks
+    def ranks(self) -> tuple[list]:
+        board = self.board
+        top_rank = []
+        middle_rank = []
+        bottom_rank = []
+        for i in range(0, 9):
+            row = board[i]
+            if i < 3:
+                top_rank.append(row)
+            elif i < 6:
+                middle_rank.append(row)
+            else:
+                bottom_rank.append(row)
 
-	def stacks(self):
-		blocks = self.blocks()
-		left_stack = [blocks[0], blocks[3], blocks[6]]
-		centre_stack = [blocks[1], blocks[4], blocks[7]]
-		right_stack = [blocks[2], blocks[5], blocks[8]]
-		return left_stack, centre_stack, right_stack
+        return top_rank, middle_rank, bottom_rank
 
-	def check(self,pos: tuple) -> int:
-		"""return value for any random position"""
-		x,y = pos
-		return self.board[x][y]
+    def blocks(self) -> tuple[list[list]]:
+        top_rank, middle_rank, bottom_rank = self.ranks()
+        b1 = [top_rank[0][0:3], top_rank[1][0:3], top_rank[2][0:3]]
+        b2 = [top_rank[0][3:6], top_rank[1][3:6], top_rank[2][3:6]]
+        b3 = [top_rank[0][6:9], top_rank[1][6:9], top_rank[2][6:9]]
+        b4 = [middle_rank[0][0:3], middle_rank[1][0:3], middle_rank[2][0:3]]
+        b5 = [middle_rank[0][3:6], middle_rank[1][3:6], middle_rank[2][3:6]]
+        b6 = [middle_rank[0][6:9], middle_rank[1][6:9], middle_rank[2][6:9]]
+        b7 = [bottom_rank[0][0:3], bottom_rank[1][0:3], bottom_rank[2][0:3]]
+        b8 = [bottom_rank[0][3:6], bottom_rank[1][3:6], bottom_rank[2][3:6]]
+        b9 = [bottom_rank[0][6:9], bottom_rank[1][6:9], bottom_rank[2][6:9]]
+        blocks = (
+            b1, b2, b3,
+            b4, b5, b6,
+            b7, b8, b9,
+        )
+        return blocks
+
+    def stacks(self) -> tuple[list[list]]:
+        blocks = self.blocks()
+        left_stack = [blocks[0], blocks[3], blocks[6]]
+        centre_stack = [blocks[1], blocks[4], blocks[7]]
+        right_stack = [blocks[2], blocks[5], blocks[8]]
+        return left_stack, centre_stack, right_stack
+
+    def check(self, pos: tuple) -> int:
+        """return value for any random position"""
+        x, y = pos
+        return self.board[x][y]
+
 
 class Solver(Sudoku):
     """
@@ -133,6 +150,9 @@ class Solver(Sudoku):
         self._filename = self.gen_filename()
         self.soln = self.solved()
         self.solve_status = len(self.soln) != 0
+
+    def __del__(self):
+        rm(self._filename)
 
     def gen_filename(self):
         v = string.ascii_letters + "".join([str(i) for i in range(0, 9)])
@@ -189,7 +209,6 @@ class Solver(Sudoku):
         res = []
         for i in payload:
             res.append(Sudoku(i))
-        rm(self._filename)
         return res
 
     def solve(self):
@@ -228,6 +247,7 @@ class Solver(Sudoku):
                     return
         foo()
 
+
 class Sudoku_Board_Generator:
     """
     9*9 sudoku board generator
@@ -240,8 +260,8 @@ class Sudoku_Board_Generator:
 
     @staticmethod
     def block_generator():
-        var = [i for i in range(1,10)]
-        shuffle(var) # a good mix
+        var = [i for i in range(1, 10)]
+        shuffle(var)  # a good mix
         a = []
         for _ in range(3):
             a.append(var.pop())
@@ -261,9 +281,9 @@ class Sudoku_Board_Generator:
     def initial_boardmaker(cls, blocks=None):
         if blocks == None:
             blocks = cls.block_creator()
-        zero0, one0, two0 = ([],[],[])
-        zero1, one1, two1 = ([],[],[])
-        zero2, one2, two2 = ([],[],[])
+        zero0, one0, two0 = ([], [], [])
+        zero1, one1, two1 = ([], [], [])
+        zero2, one2, two2 = ([], [], [])
         for i in range(len(blocks)):
             if i < 3:
                 for x in blocks[i]:
@@ -281,8 +301,8 @@ class Sudoku_Board_Generator:
                     one2.append(x[1])
                     two2.append(x[2])
         res = [zero0, one0, two0,
-            zero1, one1, two1,
-            zero2, one2, two2]
+               zero1, one1, two1,
+               zero2, one2, two2]
         return res
 
     def _gen(self):
@@ -306,28 +326,3 @@ class Sudoku_Board_Generator:
             foo = Solver(v)
             if foo.solve_status:
                 return foo.soln[0]
-
-# test
-board1 = [
-    [2, 4, 9, 5, 7, 6, 1, 0, 3],
-    [0, 3, 5, 4, 1, 2, 7, 6, 9],
-    [6, 7, 1, 3, 0, 9, 5, 2, 4],
-    [3, 6, 0, 2, 4, 1, 9, 5, 7],
-    [7, 9, 2, 0, 5, 3, 4, 1, 6],
-    [1, 5, 4, 6, 9, 7, 0, 3, 2],
-    [5, 1, 6, 9, 3, 4, 2, 7, 0],
-    [4, 2, 7, 1, 6, 0, 3, 9, 5],
-    [9, 0, 3, 7, 2, 5, 6, 4, 1],
-    ]
-
-board2 = [
-    [5, 3, 0, 0, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 4, 1, 9, 0, 0, 5],
-    [0, 0, 0, 0, 8, 0, 0, 0, 0],
-]
